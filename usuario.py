93d49d7ed4f963e2ccd9dd7058a867b09b6a5adb
Murillo Saldanha <99482029+offsalda@users.noreply.github.com>
2022-09-12 16:17:25 -0300

    Update usuario.py

--- usuario.py
+++ usuario.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
-app = Flask(_name_, template_folder="/Templates")
+app = Flask(__name__, template_folder="/Templates")
 #app = Flask(__name__)
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
