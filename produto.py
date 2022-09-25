d55cbfd5d85a90bb07388899951814366e9c4d61
Murillo Saldanha <99482029+offsalda@users.noreply.github.com>
2022-09-25 20:44:51 -0300

    Update produto.py

--- produto.py
+++ produto.py
@@ -9,7 +9,7 @@ app = Flask(__name__, template_folder="Templates")
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
 app.config['MYSQL_DATABASE_DB'] = 'teste'
-app.config['MYSQL_DATABASE_HOST'] = '172.18.0.1'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
 mysql.init_app(app)
 
 @app.route('/')
